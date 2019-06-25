6492f1cd77e41e55c60123a1faeb72835cac6c24
Sirghi Mihail <mr.serveen@yahoo.com>
2019-06-25 17:08:44 +0300

    Add: - future row's model;

---
+++ src/main/java/com/interview/interview/entities/TaskEntity.java
@@ -0,0 +1,44 @@
+package com.interview.interview.entities;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Table;
+
+@Table(name = "X")
+@Getter
+@Setter
+@ToString
+public class TaskEntity {
+  @Column(name = "A")
+  private String aColumn;
+
+  @Column(name = "B")
+  private String bColumn;
+
+  @Column(name = "C")
+  private String cColumn;
+
+  @Column(name = "D")
+  private String dColumn;
+
+  @Column(name = "E")
+  private String eColumn;
+
+  @Column(name = "F")
+  private String fColumn;
+
+  @Column(name = "G")
+  private String gColumn;
+
+  @Column(name = "H")
+  private boolean hColumn;
+
+  @Column(name = "I")
+  private boolean iColumn;
+
+  @Column(name = "J")
+  private String jColumn;
+}
