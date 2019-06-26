23d8ef643ebc6d2d812f864ddfd1c228a0595233
msirghi <mr.serven@yahoo.com>
2019-06-26 20:58:21 +0300

    Add: - H2 config; - db config; - log files;

---
+++ src/main/java/com/interview/interview/configs/DatabaseConfig.java
@@ -0,0 +1,74 @@
+package com.interview.interview.configs;
+
+import com.interview.interview.model.TaskModel;
+import com.interview.interview.parse.ParseFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DatabaseConfig {
+  private static Connection connection;
+  private static PreparedStatement prepStatement;
+  private static final Logger LOGGER = LoggerFactory.getLogger(ParseFile.class);
+  private static final String DATABASE_DRIVER = "org.h2.Driver";
+  private static final String DATABASE_CONNECTION = "jdbc:h2:mem:testdb";
+  private static final String DATABASE_USER = "sa";
+  private static final String DATABASE_PASSWORD = "secret";
+  private static final String sqlQuery = "INSERT INTO X (A, B, C, D, E, F, G, H, I, J) " +
+          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+  public DatabaseConfig() {
+    connectDB();
+    getDBConnection();
+  }
+
+  private static Connection getDBConnection() {
+    Connection H2DBConnection = null;
+    try {
+      Class.forName(DATABASE_DRIVER);
+      H2DBConnection = DriverManager
+              .getConnection(DATABASE_CONNECTION, DATABASE_USER, DATABASE_PASSWORD);
+      return H2DBConnection;
+    } catch (ClassNotFoundException ex) {
+      LOGGER.warn(ex.toString());
+    } catch (SQLException ex) {
+      LOGGER.warn("Error connecting to db. Verify data.");
+    }
+    return H2DBConnection;
+  }
+
+  public static void connectDB() {
+    connection = getDBConnection();
+    try {
+      connection.setAutoCommit(false);
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void performSqlStatement(TaskModel taskModel) {
+    try {
+      prepStatement = connection.prepareStatement(sqlQuery);
+      prepStatement.setString(1, taskModel.getAColumn());
+      prepStatement.setString(2, taskModel.getBColumn());
+      prepStatement.setString(3, taskModel.getCColumn());
+      prepStatement.setString(4, taskModel.getDColumn());
+      prepStatement.setString(5, taskModel.getEColumn());
+      prepStatement.setString(6, taskModel.getFColumn());
+      prepStatement.setString(7, taskModel.getGColumn());
+      prepStatement.setString(8, taskModel.getHColumn());
+      prepStatement.setString(9, taskModel.getIColumn());
+      prepStatement.setString(10, taskModel.getJColumn());
+      prepStatement.executeUpdate();
+      prepStatement.close();
+      connection.commit();
+    } catch (SQLException e) {
+      LOGGER.warn("Error while executing sql statement.");
+    }
+  }
+
+}
--- src/main/java/com/interview/interview/parse/ParseFile.java
+++ src/main/java/com/interview/interview/parse/ParseFile.java
@@ -1,5 +1,6 @@
 package com.interview.interview.parse;
 
+import com.interview.interview.configs.DatabaseConfig;
 import com.interview.interview.model.TaskModel;
 import com.opencsv.CSVReader;
 import lombok.ToString;
@@ -9,105 +10,59 @@ import org.slf4j.LoggerFactory;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.sql.*;
 
 @ToString
 public class ParseFile {
   private CSVReader reader;
   private long recordsReceived;
   private long recordsFailed;
-  private Connection connection;
-  private PreparedStatement prepStatement;
-
+  private DatabaseConfig databaseConfig;
   private static final Logger LOGGER = LoggerFactory.getLogger(ParseFile.class);
-  private static final String DATABASE_DRIVER = "org.h2.Driver";
-  private static final String DATABASE_CONNECTION = "jdbc:h2:mem:testdb";
-  private static final String DATABASE_USER = "sa";
-  private static final String DATABASE_PASSWORD = "secret";
-  private static final String sqlQuery = "INSERT INTO X (A, B, C, D, E, F, G, H, I, J) " +
-          "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
   public ParseFile(String path) {
     try {
       reader = new CSVReader(new FileReader(path), ',');
       reader.readNext();
+      databaseConfig = new DatabaseConfig();
     } catch (IOException e) {
       LOGGER.error("File not found.");
     }
   }
 
-  private static Connection getDBConnection() {
-    Connection H2DBConnection = null;
-    try {
-      Class.forName(DATABASE_DRIVER);
-      H2DBConnection = DriverManager
-              .getConnection(DATABASE_CONNECTION, DATABASE_USER, DATABASE_PASSWORD);
-      return H2DBConnection;
-    } catch (ClassNotFoundException ex) {
-      LOGGER.warn(ex.toString());
-    } catch (SQLException ex) {
-      LOGGER.warn("Error connecting to db. Verify data.");
-    }
-    return H2DBConnection;
-  }
-
   public void parseCvs() throws Exception {
-    String[] nextLine;
-    String[] tempLine;
+    String[] row;
+    String[] tempRow;
     FileWriter csvWriter = new FileWriter("bad-data-"+ System.currentTimeMillis() + ".csv");
-    connection = getDBConnection();
-    connection.setAutoCommit(false);
 
-    while ((nextLine = reader.readNext()) != null) {
+    while ((row = reader.readNext()) != null) {
       recordsReceived++;
-      int j = 1;
-      tempLine = nextLine;
-      TaskModel taskModel = new TaskModel();
+      int goodRecordCounter = 1;
+      tempRow = row;
+      TaskModel record = new TaskModel();
       try {
-        for (String token : nextLine) {
-          emptyCheck(token);
-          setModelField(taskModel, j++, token);
+        for (String column : row) {
+          emptyCheck(column);
+          setModelField(record, goodRecordCounter++, column);
         }
-        performSqlStatement(taskModel);
+        databaseConfig.performSqlStatement(record);
       } catch (IllegalAccessException e) {
         recordsFailed++;
-        int q = 1;
-        TaskModel taskModel1 = new TaskModel();
-        for (String token : tempLine) {
-          setModelField(taskModel1, q++, token);
+        int badRecordColumn = 1;
+        TaskModel badRecord = new TaskModel();
+        for (String token : tempRow) {
+          setModelField(badRecord, badRecordColumn++, token);
         }
-        csvWriter.append(String.join(",", taskModel1.toString()));
+        csvWriter.append(String.join(",", badRecord.toString()));
         csvWriter.append("\n");
         csvWriter.flush();
       }
     }
-    LOGGER.info("Records received: " + recordsReceived);
-    LOGGER.info("Records successful: " + (recordsReceived - recordsFailed));
-    LOGGER.info("Records failed: " + recordsFailed);
+    LOGGER.error("Records received: " + recordsReceived);
+    LOGGER.error("Records successful: " + (recordsReceived - recordsFailed));
+    LOGGER.error("Records failed: " + recordsFailed);
 
     csvWriter.close();
     reader.close();
-  }
-
-  private void performSqlStatement(TaskModel taskModel) {
-    try {
-      prepStatement = connection.prepareStatement(sqlQuery);
-      prepStatement.setString(1, taskModel.getAColumn());
-      prepStatement.setString(2, taskModel.getBColumn());
-      prepStatement.setString(3, taskModel.getCColumn());
-      prepStatement.setString(4, taskModel.getDColumn());
-      prepStatement.setString(5, taskModel.getEColumn());
-      prepStatement.setString(6, taskModel.getFColumn());
-      prepStatement.setString(7, taskModel.getGColumn());
-      prepStatement.setString(8, taskModel.getHColumn());
-      prepStatement.setString(9, taskModel.getIColumn());
-      prepStatement.setString(10, taskModel.getJColumn());
-      prepStatement.executeUpdate();
-      prepStatement.close();
-      connection.commit();
-    } catch (SQLException e) {
-      LOGGER.warn("Error while executing sql statement.");
-    }
   }
 
   private static void setModelField(TaskModel taskModel, Integer j, String token) {
